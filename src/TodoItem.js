8f6388799d761816dae97d31d2d132ce14ad8d3f
Nachiket <nks5@illinois.edu>
2024-05-05 19:27:24 -0700

    Update TodoItem.js

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,29 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 // This component will represent a single todo item
 
 // In the TodoItem component, you can handle click events to toggle the completion status or remove a todo.
 //TODO: Implement method to toggle completion of todos via strikethrough 
 export default function TodoItem(item, clickedItems, setClickedItems) {
-    console.log(item);
-    let isStrikeThrough = clickedItems.includes(item);
-
-    const handleRemoveItem = (item) => {
-        setClickedItems((clickedItems) => clickedItems.filter(function(element) {
-            return element !== item;
-        }));
-      }
-
-    // strikethrough item on click and add it to clickedItems
-    // if already strikethroughed, remove from clickedItems
     const handleClick = (event) => {
         event.preventDefault();
         setClickedItems(prevClickedItems => {
             if (prevClickedItems.includes(item)) {
+                // if todo is already strikethroughed (clicked on), remove from clickedItems
+                console.log('remove item: ' + item);
                 return prevClickedItems.filter(i => i !== item);
             } else {
+                // Else, add item to clickedItems
+                console.log('add item: ' + item);
                 return [...prevClickedItems, item];
             }
         });
+        console.log(`clicked items: ${clickedItems}`);
     }
 
     return (
